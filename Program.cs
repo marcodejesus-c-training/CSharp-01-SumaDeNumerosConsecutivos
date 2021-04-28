9303b5a07b1700ec060ae4c62c1a30e77582970a
Marco De Jesus <marco_dejesus@live.com>
2021-04-28 03:54:00 -0500

    Implementacion de Numeros Consecutivos

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp_01_SumaDeNumerosConsecutivos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 1;
+            int suma = 0;
+
+            while(contador <= 10){
+                suma = contador + suma;
+                contador++;
+            }
+
+            Console.WriteLine("El Total de la Suma es: " + suma);
+        }
+    }
+}
